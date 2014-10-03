ed14390946fa5eb78cae6c13520ba3628b30d3f9
mikera <mike.r.anderson.13@gmail.com>
2014-10-03 16:34:41 +0800

    with-component docs

--- src/main/clojure/trellis/component.clj
+++ src/main/clojure/trellis/component.clj
@@ -4,6 +4,8 @@
   (:use [mikera.cljutils.error]))
 
 (defmacro with-component
+  "Performs the operations specified by body after starting the given component and binding it to a symbol. Stops
+   the component once complete (in a finally clause)."
   [[sym comp :as v] & body]
   (when-not (and (vector? v) (== 2 (count v))) (error "with-component requires a 2-element binding vector"))
   (when-not (symbol? sym) (error "with-component requires a symbol to bind"))
