d17d96d70c5e12d4c25d595aa994d4edc29c5f33
mikera <mike.r.anderson.13@gmail.com>
2014-10-08 12:55:26 +0800

    refactor to use start/stop for sente server

--- src/main/clojure/trellis/sente.clj
+++ src/main/clojure/trellis/sente.clj
@@ -1,6 +1,7 @@
 (ns trellis.sente
   (:require
     [clojure.tools.logging :as log]
+    [clojure.core.async :as async :refer []]
     [com.stuartsierra.component :as component]
     [taoensso.sente :as sente]
     ))
@@ -10,20 +11,36 @@
                         ajax-post-fn 
                         ajax-get-or-ws-handshake-fn 
                         connected-uids]
-  )
+  component/Lifecycle
+	  (start [s]
+	    (if (:ch-recv s)
+         s
+         (do ;; start sente component
+           (let [csr (sente/make-channel-socket! {})]
+            (merge s csr)))))
+	  (stop [s]
+	    (if (:ch-recv s)
+        (do ;; stop sente component
+          (sente-server))
+        s)))
 
 (defn sente-server 
   "Returns a sente server-side component. 
    The ajax functions are intented to be used as ring handlers. " []
-  (let [{:keys [ch-recv ; ChannelSocket's receive channel
-                send-fn ; ChannelSocket's send API fn
-                ajax-post-fn ; function for Ring handler POST 
-                ajax-get-or-ws-handshake-fn ; function for Ring handler GET
-                connected-uids ; Watchable, read-only atom
-                ]}
-        (sente/make-channel-socket! {})]
-     (SenteServer. ch-recv 
-                   send-fn 
-                   ajax-post-fn 
-                   ajax-get-or-ws-handshake-fn 
-                   connected-uids)))
+  (SenteServer. nil nil nil nil nil))
+
+;(defn sente-server 
+;  "Returns a sente server-side component. 
+;   The ajax functions are intented to be used as ring handlers. " []
+;  (let [{:keys [ch-recv ; ChannelSocket's receive channel
+;                send-fn ; ChannelSocket's send API fn
+;                ajax-post-fn ; function for Ring handler POST 
+;                ajax-get-or-ws-handshake-fn ; function for Ring handler GET
+;                connected-uids ; Watchable, read-only atom
+;                ] :as csr}
+;        (sente/make-channel-socket! {})]
+;     (SenteServer. ch-recv 
+;                   send-fn 
+;                   ajax-post-fn 
+;                   ajax-get-or-ws-handshake-fn 
+;                   connected-uids)))
