f5b6c482d515cec51698262e2e9aaae26e7f017b
mikera <mike.r.anderson.13@gmail.com>
2014-11-02 19:56:31 +0800

    update deps

--- project.clj
+++ project.clj
@@ -3,30 +3,30 @@
   :url "http://github.com/mikera/trellis"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.9"]
+                 [org.clojure/tools.reader "0.8.11"]
                  [org.clojure/tools.nrepl "0.2.6"]
                  
                  ;; CLJ
                  [ring/ring-core "1.3.1"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.clojure/java.jdbc "0.3.6"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-time "0.8.0"]
                  [ring-cors "0.1.4"]
-                 [compojure "1.2.0"]
+                 [compojure "1.2.1"]
                  [liberator "0.12.2"]
                  [cheshire "5.3.1"]
                  [net.mikera/clojure-utils "0.6.1"]
                  [javax.servlet/servlet-api "2.5"]        
                  [com.stuartsierra/component "0.2.2"]
-                 [prismatic/schema "0.3.1"]
+                 [prismatic/schema "0.3.2"]
                  
                  ;; CLJS
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [secretary "1.2.1"]
-                 [markdown-clj "0.9.55"]
-                 [cljs-http "0.1.18"]
+                 [markdown-clj "0.9.55" :exclusions [org.clojure/clojure]]
+                 [cljs-http "0.1.19"]
                  [om "0.7.3"]
                  [http-kit "2.1.19"]
                  
