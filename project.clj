740b572fdf16c6189719a5c36f76ef27f38e5e4b
Mike <mike@mikera.net>
2015-05-25 13:48:45 +0800

    update deps

--- project.clj
+++ project.clj
@@ -2,37 +2,37 @@
   :description "Trellis Web Framework"
   :url "http://github.com/mikera/trellis"
 
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [org.clojure/tools.reader "0.9.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/tools.reader "0.9.2"]
                  [org.clojure/tools.nrepl "0.2.10"]
                  
                  ;; CLJ
                  [ring/ring-core "1.3.2"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/java.jdbc "0.3.6"]
+                 [org.clojure/java.jdbc "0.3.7"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-time "0.9.0"]
                  [ring-cors "0.1.7"]
-                 [compojure "1.3.3"]
-                 [liberator "0.12.2"]
+                 [compojure "1.3.4"]
+                 [liberator "0.13"]
                  [cheshire "5.4.0"]
                  [net.mikera/clojure-utils "0.6.1"]
                  [javax.servlet/servlet-api "2.5"]        
                  [com.stuartsierra/component "0.2.3"]
-                 [prismatic/schema "0.4.0"]
+                 [prismatic/schema "0.4.3"]
                  [environ "1.0.0"]
                  
                  ;; CLJS
-                 [org.clojure/clojurescript "0.0-3196"]
+                 [org.clojure/clojurescript "0.0-3297"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [secretary "1.2.3"]
-                 [markdown-clj "0.9.65" :exclusions [org.clojure/clojure]]
+                 [markdown-clj "0.9.66" :exclusions [org.clojure/clojure]]
                  [cljs-http "0.1.30"]
                  [om "0.7.3"]
                  [http-kit "2.1.19"]
                  
                  ;; Both CLJ and CLJS
-                 [com.taoensso/sente "1.3.0"]]
+                 [com.taoensso/sente "1.4.1"]]
   
   :profiles {:dev {:dependencies [[com.h2database/h2 "1.4.187"]
                                   [net.mikera/cljunit "0.3.1"]]
